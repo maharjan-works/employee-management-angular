488006f2270145ebf9f20ed22aae9b3d28aeac03
maharjan.works <maharjan.works@gmail.com>
2023-11-26 17:16:00 -0500

    employee-list component modified

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
+  employeeList: Employee[] | undefined;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.employeeList = [{
+      "employeeId":"EMP101",
+      "firstName":"Joe",
+      "lastName": "Biden",
+      "emailId": "joe.biden@gmail.com"
+    },
+    {
+      "employeeId":"EMP102",
+      "firstName":"Kamala",
+      "lastName": "Harris",
+      "emailId": "kamala.harris@gmail.com"
+    }
+
+    ]
   }
 
 }
--- src/app/employee.ts
+++ src/app/employee.ts
@@ -1,6 +1,6 @@
 export class Employee {
-    employeeId!: string;
-    firstName!: string;
-    lastName!: string;
-    emailId!: string;
+    employeeId: string | undefined;
+    firstName: string | undefined;
+    lastName: string | undefined;
+    emailId: string | undefined;
 }
