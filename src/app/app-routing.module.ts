876045f18ee9bf318c19e3f9098b3ee616abf64e
maharjan.works <maharjan.works@gmail.com>
2023-11-26 18:54:03 -0500

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "employeeList", component: EmployeeListComponent},
+  {path: '', redirectTo: 'employeeList', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
