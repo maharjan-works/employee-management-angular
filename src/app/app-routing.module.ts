7a8e4b675de3fe41f9cc87e0c4e5d95f74738e9e
maharjan.works <maharjan.works@gmail.com>
2023-11-26 19:14:20 -0500

    create-employee component created, added routing for it & Add Employee list is in nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 const routes: Routes = [
   {path: "employeeList", component: EmployeeListComponent},
-  {path: '', redirectTo: 'employeeList', pathMatch: 'full'}
+  {path: '', redirectTo: 'employeeList', pathMatch: 'full'},
+  {path: 'create-employee', component: CreateEmployeeComponent}
 ];
 
 @NgModule({
