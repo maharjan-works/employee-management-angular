420166790e2b19a8ac96c3614a32b0552b4c00d5
maharjan.works <maharjan.works@gmail.com>
2023-11-29 20:54:32 -0500

    update-employee component created, set route, and created button in employee-list html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 const routes: Routes = [
   {path: "employeeList", component: EmployeeListComponent},
   {path: '', redirectTo: 'employeeList', pathMatch: 'full'},
-  {path: 'create-employee', component: CreateEmployeeComponent}
+  {path: 'create-employee', component: CreateEmployeeComponent},
+  {path: 'update-employee/:employeeId', component: UpdateEmployeeComponent}
 ];
 
 @NgModule({
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
@@ -11,7 +12,7 @@ export class EmployeeListComponent implements OnInit {
 
   employeeList: Employee[] = [];
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService, private router:Router) { }
 
   ngOnInit(): void {
    this.getEmployeeList();
@@ -21,6 +22,14 @@ export class EmployeeListComponent implements OnInit {
     this.employeeService.getEmployeeList().subscribe(data => {
       this.employeeList = data;
     });
+  }
+
+  updateEmployee(employeeId: string){
+    this.router.navigate([`update-employee`, employeeId]);
+  }
+
+  deleteEmployee(employeeId: string){
+    this.router.navigate(['delete-employee',employeeId])
   }
 
 }
